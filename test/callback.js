20048fc5f9571840d3f89d23c2d55e2dbedfc7dc
Стас Ляшевский <liashevskiys@gmail.com>
2022-11-02 23:37:28 +0100

    callback objectES6 trim 30-32

---
+++ test/callback.js
@@ -0,0 +1,58 @@
+"use strict"
+
+function first() {
+	setTimeout(function () {
+		console.log(1);
+	}, 500);
+
+}
+
+
+function second() {
+	console.log(2);
+}
+
+
+
+
+first();
+second();
+
+
+//
+
+function learnJS(lang, callback) {
+	console.log(`Я учу: ${lang}`);
+	callback();
+}
+
+
+
+
+
+learnJS('JavaScript',function() {
+	console.log('Я прошел этот урок!');
+} );
+
+
+//
+
+
+
+//
+
+
+function learnJS(lang, callback) {
+	console.log(`Я учу: ${lang}`);
+	callback();
+}
+
+function done() {
+	console.log('Я прошел этот урок!');
+}
+
+
+learnJS('JavaScript',done);
+
+
+//
