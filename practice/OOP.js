3421d8be681f091b57f942f1bb7fcf7198cb2467
Стас Ляшевский <liashevskiys@gmail.com>
2022-11-12 14:48:32 +0100

    OOP 36

--- practice/OOP.js
+++ practice/OOP.js
@@ -1,10 +1,43 @@
 "use strict"
 
-
 let str = "some";
 let strObj = new String(str);
 
-console.log(str);
-console.log(strObj);
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function() {
+		console.log("Hello");
+	}
+};
+
+
+
+const jonh = Object.create(soldier);
+
+
+
+
+// const jonh = {
+// 	health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+
+
+
+
+//
+console.log(jonh.armor);
+jonh.sayHello();
+
 
 
