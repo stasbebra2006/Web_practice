e19d3fb7629804b373f9f3709f6e5039b3da1a91
stasbebra2006 <liashevskiys@gmail.com>
2023-02-04 22:08:12 +0100

    67_Dates

---
+++ Section_4/67_Dates/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const now = new Date('2020-05-01');
+// new Date.parse('2020-05-01');
+
+console.log(now.setHours(18, 40));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+//hour,seconds,milliseconds,...
+
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+
+let start = new Date();
+
+for(let i = 0;i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+console.log(`Цикл отработал за ${end - start} миллисекунд`);
