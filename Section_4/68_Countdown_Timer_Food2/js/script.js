285f1d1391833db2c6bc9552b791f5446407cdaa
stasbebra2006 <liashevskiys@gmail.com>
2023-02-08 00:38:04 +0100

    68_Timer+69_Adjustments

--- Section_4/68_Countdown_Timer_Food2/js/script.js
+++ Section_4/68_Countdown_Timer_Food2/js/script.js
@@ -41,42 +41,53 @@ window.addEventListener('DOMContentLoaded', function() {
     
     // Timer
 
-    const deadline = '2022-06-11';
+    const deadline = '2023-02-09';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor( (t/(1000*60*60*24)) ),
-            seconds = Math.floor( (t/1000) % 60 ),
-            minutes = Math.floor( (t/1000/60) % 60 ),
-            hours = Math.floor( (t/(1000*60*60) % 24) );
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
 
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
+        if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60); 
+
+            // return {
+            //     'total': t,
+            //     'days': days,
+            //     'hours': hours,
+            //     'minutes': minutes,
+            //     'seconds': seconds
+            // };
+        }
+            return {
+                total: t, days, hours, minutes, seconds
+            }
     }
 
-    function getZero(num){
-        if (num >= 0 && num < 10) { 
-            return '0' + num;
-        } else {
-            return num;
-        }
+    function getZero(num) {
+        if(num >= 0 && num < 10) {
+            return `0${num}`;
+        } else num;
     }
 
     function setClock(selector, endtime) {
-
         const timer = document.querySelector(selector),
-            days = timer.querySelector("#days"),
+            days = timer.querySelector('#days'),
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
-
+        
         updateClock();
+
+
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
@@ -86,12 +97,11 @@ window.addEventListener('DOMContentLoaded', function() {
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0) {
+            if(t.total <= 0) {
                 clearInterval(timeInterval);
             }
         }
     }
 
     setClock('.timer', deadline);
-
 });
