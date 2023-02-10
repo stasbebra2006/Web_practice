7cd1e0132298a05e2e2e7d39fb898a9ce277e38d
stasbebra2006 <liashevskiys@gmail.com>
2023-02-11 00:05:24 +0100

    72_MutationObserve

---
+++ Section_4/72_MutationObserver_ResizeObserver_contenteditable/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(MutationRecord => {
+    console.log(MutationRecord);
+});
+
+observer.observe(box, {
+    childList: true
+});
+
+// observer.disconnect(observer);
