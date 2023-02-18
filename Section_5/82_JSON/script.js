3f049ab75980705ad577a2fa80520c1c59f3a5cd
stasbebra2006 <liashevskiys@gmail.com>
2023-02-18 20:42:27 +0100

    JSON + deep copy

---
+++ Section_5/82_JSON/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const persone = {
+    name: 'stasbebra2006',
+    tel: '+sasaaaaat',
+    parents: {
+        mom: 'Srat',
+        dad: 'Bro'
+    }
+}; 
+
+const clone = JSON.parse(JSON.stringify(persone));
+
+clone.parents.mom = 'Ann';
+
+
+console.log(persone);
+console.log(clone);
+
